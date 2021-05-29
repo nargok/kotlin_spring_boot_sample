124ad4b0d6b6312b1aea23146dc1e8883abc8924
nargok <nargok@gmail.com>
2021-05-30 01:07:47 +0900

    path parameter

--- src/main/kotlin/com/example/demo/GreeterController.kt
+++ src/main/kotlin/com/example/demo/GreeterController.kt
@@ -1,15 +1,17 @@
 package com.example.demo
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("greeter")
 class GreeterController {
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
+        return HelloResponse("Hello ${name}")
+    }
+
+    @GetMapping("/hello/{name}")
+    fun helloPathValue(@PathVariable("name") name: String): HelloResponse {
         return HelloResponse("Hello ${name}")
     }
 }
